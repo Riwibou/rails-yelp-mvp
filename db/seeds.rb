029d14ed9058e21e3a98490f96182e26fde0a50d
Riwan William Bououli <riwan.b@live.fr>
2023-02-22 20:24:32 +0100

    rails yelp mvp not done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create(name: "Kebap", address: "10 rue de la boustifaille", phone_number: "0606060606", category: "french" )
+Restaurant.create(name: "La bonne graille", address: "24 avenue de bouff", phone_number: "0206060606", category: "french" )
+Restaurant.create(name: "Akatsuki", address: "54 rue de l'opera", phone_number: "0306060606", category: "japanese" )
+Restaurant.create(name: "Dolce Vita", address: "189 rue de Rome", phone_number: "0406060606", category: "italian" )
+Restaurant.create(name: "Miàn Miam", address: "92 rue de choisy", phone_number: "0506060606", category: "chinese" )
